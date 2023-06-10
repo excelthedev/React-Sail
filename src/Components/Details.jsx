16640140175edb14c4c108efc668381aeb32465e
theseventhgod <tobyadeyy@gmail.com>
2023-06-10 14:14:18 +0100

    box-shadow

--- src/Components/Details.jsx
+++ src/Components/Details.jsx
@@ -6,7 +6,7 @@ const Details = (props) => {
     <>
       <div className="container">
         <div className="self-con">
-          <p>FirstName: {props.firstname}</p>
+          <p className="para-fine">FirstName: {props.firstname}</p>
           <p>LastName: {props.lastname}</p>
           <p>Email Address: {props.email}</p>
           <p>Phone Number: {props.phone}</p>
